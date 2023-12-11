04e30fea66f19504470860c8891a5da31905c343
jojusuar <jojusuar@espol.edu.ec>
2023-12-11 15:10:05 -0500

    creacion del proyecto

---
+++ src/main/java/com/espol/carpool_espol/Carpool_ESPOL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espol.carpool_espol;
+
+/**
+ *
+ * @author euclasio
+ */
+public class Carpool_ESPOL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
