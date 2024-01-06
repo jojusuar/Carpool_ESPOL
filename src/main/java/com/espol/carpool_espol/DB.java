09673f449162c44729e92d225a2f4ab5c08e5c34
jojusuar <jojusuar@espol.edu.ec>
2024-01-05 23:13:56 -0500

    metodos de instanciacion de tablas

--- src/main/java/com/espol/carpool_espol/DB.java
+++ src/main/java/com/espol/carpool_espol/DB.java
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -40,8 +38,8 @@ public class DB {
             startConnection();
         }
     }
-    
-    public static void closeConnection(){
+
+    public static void closeConnection() {
         try {
             connection.close();
         } catch (SQLException ex) {
@@ -171,23 +169,304 @@ public class DB {
         try {
             // Query parametrizado para evitar ataques de SQL injection
             String insertQuery = "insert into soportealcliente (idempleado) VALUES (?)";
-            System.out.println("Ingrese un correo electrónico: ");
+            System.out.println("Ingrese el ID del empleado: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setInt(1, id);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newCarModel(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into modeloauto VALUES (?, ?)";
+            System.out.println("Ingrese el modelo del auto: ");
+            String model = scanner.nextLine();
+            System.out.println("Ingrese el color del auto: ");
+            String color = scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, model);
+                preparedStatement.setString(2, color);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newRoute(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into ruta VALUES (?, ?, ?)";
+            System.out.println("Ingrese el ID de la ruta: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el lugar de origen: ");
+            String source = scanner.nextLine();
+            System.out.println("Ingrese el lugar de destino: ");
+            String target = scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setInt(1, id);
+                preparedStatement.setString(2, source);
+                preparedStatement.setString(3, target);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newStop(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into paradas(ubicacionparada, idruta) VALUES (?, ?)";
+            System.out.println("Ingrese el ID de la ruta: ");
+            int routeid = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el lugar de la parada: ");
+            String location = scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, location);
+                preparedStatement.setInt(2, routeid);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newReview(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into resena VALUES (?, ?, ?, ?)";
+            System.out.println("Ingrese el correo del usuario a reseñar: ");
+            String reviewed = scanner.nextLine();
+            System.out.println("Ingrese el correo de quien escribe la reseña: ");
+            String reviewer = scanner.nextLine();
+            System.out.println("Ingrese el ID de la reseña: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese la reseña: ");
+            String review = scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, reviewed);
+                preparedStatement.setString(2, reviewer);
+                preparedStatement.setInt(3, id);
+                preparedStatement.setString(4, review);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newCarInfo(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into infoauto VALUES (?, ?, ?, ?)";
+            System.out.println("InfoAuto");
+            System.out.println("Ingrese el correo del conductor: ");
             String email = scanner.nextLine();
-            System.out.println("Ingrese una contraseña: ");
-            String contrasena = scanner.nextLine();
-            System.out.println("Ingrese sus nombres: ");
-            String name = scanner.nextLine();
-            System.out.println("Ingrese sus apellidos: ");
-            String lastname = scanner.nextLine();
-            System.out.println("Ingrese un número telefónico: ");
-            String phone = scanner.nextLine();
+            System.out.println("Ingrese la placa del auto: ");
+            String placa = scanner.next();
+            System.out.println("Ingrese el modelo: ");
+            String modelo = scanner.next();
+            System.out.println("Ingrese Nro. Chasis: ");
+            String nroChasis = scanner.next();
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
                 preparedStatement.setString(1, email);
-                preparedStatement.setString(2, contrasena);
-                preparedStatement.setString(3, name);
-                preparedStatement.setString(4, lastname);
-                preparedStatement.setString(5, phone);
+                preparedStatement.setString(2, placa);
+                preparedStatement.setString(3, modelo);
+                preparedStatement.setString(4, nroChasis);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newSupportTicket(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into ticketsoporte VALUES (?, ?, ?)";
+            System.out.println("Ingrese el ID del empleado: ");
+            int idemployee = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el email del usuario: ");
+            String email = scanner.nextLine();
+            System.out.println("Ingrese el lugar de destino: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setInt(1, idemployee);
+                preparedStatement.setString(2, email);
+                preparedStatement.setInt(3, id);
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newTrip(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into viaje VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            System.out.println("Ingrese el correo del conductor: ");
+            String email = scanner.nextLine();
+            System.out.println("Ingrese el ID del viaje: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el ID de la ruta: ");
+            int routeid = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el precio: ");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Ingrese la tarifa: ");
+            double tax = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Ingrese la hora de partida (hh:mm:ss): ");
+            String hour = scanner.nextLine();
+            System.out.println("Ingrese el estado del viaje: ");
+            String status = scanner.nextLine();
+            System.out.println("Ingrese los asientos disponibles: ");
+            int seats = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese la novedad: ");
+            String hiccups = scanner.nextLine();
+            System.out.println("Ingrese la fecha del viaje (yyyy-mm-dd): ");
+            String date = scanner.nextLine();
+            System.out.println("Ingrese preferencias del viaje: ");
+            String preferences = scanner.nextLine();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, email);
+                preparedStatement.setInt(2, id);
+                preparedStatement.setInt(3, routeid);
+                preparedStatement.setDouble(4, price);
+                preparedStatement.setDouble(5, tax);
+                preparedStatement.setString(6, hour);
+                preparedStatement.setString(7, status);
+                preparedStatement.setInt(8, seats);
+                preparedStatement.setString(9, hiccups);
+                preparedStatement.setString(10, date);
+                preparedStatement.setString(11, preferences);
+
+                // Ejecuta la inserción
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                // devuelve el número de entradas añadidas
+                if (rowsAffected > 0) {
+                    System.out.println("Insert successful. Rows affected: " + rowsAffected);
+                } else {
+                    System.out.println("Insert failed.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void newReservation(Scanner scanner) {
+        scanner.nextLine();
+        try {
+            // Query parametrizado para evitar ataques de SQL injection
+            String insertQuery = "insert into reservacion VALUES (?, ?, ?, ?, ?)";
+            System.out.println("Ingrese email: ");
+            String email = scanner.next();
+            System.out.println("Ingrese ID viaje: ");
+            int idViaje = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese ID reserva: ");
+            int idRes = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el detalle de la reserva: ");
+            String detalle = scanner.next();
+            System.out.println("Ingrese fecha de la reserva (yyyy-mm-dd): ");
+            String fecha = scanner.next();
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                preparedStatement.setString(1, email);
+                preparedStatement.setInt(2, idViaje);
+                preparedStatement.setInt(3, idRes);
+                preparedStatement.setString(4, detalle);
+                preparedStatement.setString(5, fecha);
 
                 // Ejecuta la inserción
                 int rowsAffected = preparedStatement.executeUpdate();
