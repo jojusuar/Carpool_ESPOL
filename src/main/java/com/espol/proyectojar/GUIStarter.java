71732ca7baa35211c8e709b62c23edf83a4d39e9
Jazarumag <joshzaruma@gmail.com>
2024-01-06 09:45:38 -0500

    App sin conectar

---
+++ src/main/java/com/espol/proyectojar/GUIStarter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectojar;
+
+/**
+ *
+ * @author joshz
+ */
+public class GUIStarter {
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+}
---
+++ src/main/java/com/espol/proyectojar/InicioSesionController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectojar;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author joshz
+ */
+public class InicioSesionController {
+    
+    @FXML
+    private void initialize(){
+    }
+    
+}
