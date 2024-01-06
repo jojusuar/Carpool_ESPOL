a16d823607892d0c1ff949462fb48da55d071acc
jojusuar <jojusuar@espol.edu.ec>
2024-01-05 23:13:56 -0500

    metodos de instanciacion de tablas

--- src/main/java/com/espol/carpool_espol/BDavance2.java
+++ src/main/java/com/espol/carpool_espol/BDavance2.java
@@ -41,10 +41,10 @@ public class BDavance2 {
             opcionMenuP = s1.nextInt();
             if (opcionMenuP == 1) {
                 int opcionMenuA = 0;
-                while (opcionMenuA != 11) {
+                while (opcionMenuA != 13) {
                     System.out.println("Escoja la tabla donde añadirá datos:"
                             + "1.Soporte Al Cliente.\n"
-                            + "2. Usuario\n"
+                            + "2.Usuario\n"
                             + "3.Reseña\n"
                             + "4.InfoAuto\n"
                             + "5.TicketSoporte\n"
@@ -53,144 +53,54 @@ public class BDavance2 {
                             + "8.Paradas\n"
                             + "9.Reservacion\n"
                             + "10.Ruta\n"
-                            + "11. VOLVER");
+                            + "11.Pasajero.\n"
+                            + "12.Conductor.\n"
+                            + "13. VOLVER");
                     opcionMenuA = s1.nextInt();
 
                     //SOPORTE AL CLIENTE
-                    if (opcionMenuA == 1) {
-                        System.out.println("Soporte al cliente");
-                        System.out.println("..................");
-                        System.out.println("Ingrese ID empleado: ");
-                        int idEmp = s1.nextInt();
-                        SoporteCliente sc = new SoporteCliente(idEmp);
-                        soportes.add(sc);
-                    } // USUARIO
-                    else if (opcionMenuA == 2) {
-                        DB.newUser(s1);
+                    switch (opcionMenuA) {
+                        // USUARIO
+                        case 1:
+                            DB.newClientSupport(s1);
+                            break;
+                        //RESENA
+                        case 2:
+                            DB.newUser(s1);
+                            break;
+                        case 3:
+                            DB.newReview(s1);
+                            break;
+                        case 4:
+                            DB.newCarInfo(s1);
+                            break;
+                        case 5:
+                            DB.newSupportTicket(s1);
+                            break;
+                        case 6:
+                            DB.newCarModel(s1);
+                            break;
+                        case 7:
+                            DB.newTrip(s1);
+                            break;
+                        case 8:
+                            DB.newStop(s1);
+                            break;
+                        case 9:
+                            DB.newReservation(s1);
+                            break;
+                        case 10:
+                            DB.newRoute(s1);
+                            break;
 
-                    } //RESENA
-                    else if (opcionMenuA == 3) {
-                        System.out.println("Reseña");
-                        Resena r;
-                        System.out.println("Ingrese el usuario de origen: ");
-                        String id2 = s1.next();
-                        System.out.println("Ingrese el usuario de destinatario: ");
-                        String id1 = s1.next();
-                        System.out.println("Ingrese ID reseña: ");
-                        int idResena = s1.nextInt();
-                        System.out.println("Ingrese la reseña: ");
-                        String resena = s1.next();
-                        r = new Resena(id1, id2, idResena, resena);
-                        resenas.add(r);
-                    } else if (opcionMenuA == 4) {
-                        InfoAuto ia;
-                        System.out.println("InfoAuto");
-                        System.out.println("Ingrese ID de la cuenta: ");
-                        int id1 = s1.nextInt();
-                        System.out.println("Ingrese la placa del auto: ");
-                        String placa = s1.next();
-                        System.out.println("Ingrese el modelo: ");
-                        String modelo = s1.next();
-                        System.out.println("Ingrese Nro. Chasis: ");
-                        String nroChasis = s1.next();
-                        ia = new InfoAuto(id1, placa, modelo, nroChasis);
-                        inf.add(ia);
-                    } else if (opcionMenuA == 5) {
-                        TicketSoporte ts;
-                        System.out.println("Ticket Soporte");
-                        System.out.println("Ingrese ID empleado: ");
-                        int id1 = s1.nextInt();
-                        System.out.println("Ingrese email: ");
-                        String email = s1.next();
-                        System.out.println("Ingrese la descripción del ticket: ");
-                        String desc = s1.next();
-                        ts = new TicketSoporte(id1, email, desc);
-                        tickets.add(ts);
-
-                    } else if (opcionMenuA == 6) {
-                        ModeloAuto ma;
-                        System.out.println("Modelo Auto");
-                        System.out.println("Ingrese el modelo: ");
-                        String modelo = s1.next();
-                        System.out.println("Ingrese el color del Vehiculo: ");
-                        String color = s1.next();
-
-                        ma = new ModeloAuto(modelo, color);
-                        modelos.add(ma);
-                    } else if (opcionMenuA == 9) {
-                        Reservacion r;
-                        System.out.println("Reservacion");
-                        System.out.println("Ingrese email: ");
-                        String email = s1.next();
-                        System.out.println("Ingrese ID viaje: ");
-                        int idViaje = s1.nextInt();
-                        System.out.println("Ingrese ID reserva: ");
-                        int idRes = s1.nextInt();
-                        System.out.println("Ingrese el detalle de la reserva: ");
-                        String detalle = s1.next();
-                        System.out.println("Ingrese fecha de la reserva: ");
-                        String fecha = s1.next();
-
-                        r = new Reservacion(email, idViaje, idRes, detalle, fecha);
-                        reser.add(r);
-                    } else if (opcionMenuA == 7) {
-                        Viaje v;
-
-                        System.out.println("Viaje");
-                        System.out.println("Ingrese el ID del conductor: ");
-                        int idCond = s1.nextInt();
-                        System.out.println("Ingrese el ID del viaje: ");
-                        int idViaje = s1.nextInt();
-                        System.out.println("Ingrese el ID de la ruta: ");
-                        int idRuta = s1.nextInt();
-                        System.out.println("Ingrese el precio: ");
-                        double precio = s1.nextDouble();
-                        System.out.println("Ingrese la tarifa: ");
-                        double tarifa = s1.nextDouble();
-                        System.out.println("Ingrese la hora: ");
-                        String hora = s1.next();
-                        System.out.println("Ingrese el estado: ");
-                        Estado estado = Estado.valueOf(s1.next());
-                        System.out.println("Asientos disponibles: ");
-                        int asientos = s1.nextInt();
-                        System.out.println("Ingrese la novedad: ");
-                        String novedad = s1.next();
-                        System.out.println("Ingrese la fecha (DD/MM/AA): ");
-                        String fecha = s1.next();
-                        System.out.println("Ingrese preferencias: ");
-                        String preferencias = s1.next();
-
-                        v = new Viaje(idCond, idViaje, idRuta, precio, tarifa, hora, estado, asientos, novedad, fecha, preferencias);
-                        viajes.add(v);
-                    } else if (opcionMenuA == 10) {
-                        Ruta r;
-                        System.out.println("Ruta");
-
-                        System.out.println("Ingrese ID ruta: ");
-                        int idRuta = s1.nextInt();
-                        System.out.println("Ingrese el origen de la ruta: ");
-                        String origen = s1.next();
-                        System.out.println("Ingrese el destino de la ruta: ");
-                        String destino = s1.next();
-
-                        r = new Ruta(idRuta, origen, destino);
-                        rutas.add(r);
-                    } else if (opcionMenuA == 8) {
-                        Parada p;
-                        System.out.println("Parada");
-                        ArrayList<String> paradasArray = new ArrayList<>();
-
-                        System.out.println("Ingrese la ID de la ruta: ");
-                        int idRuta = s1.nextInt();
-                        System.out.println("Ingrese el # de paradas: ");
-                        int b = s1.nextInt();
-                        for (int i = 1; i < b + 1; i++) {
-                            System.out.println("Ingrese la parada #" + i + ": ");
-                            String parada1 = s1.next();
-                            paradasArray.add(parada1);
-                        }
-                        p = new Parada(idRuta, paradasArray);
-                        paradas.add(p);
+                        case 11:
+                            DB.addCreditCard(s1);
+                            break;
+                        case 12:
+                            DB.addBankAccount(s1);
+                            break;
+                        default:
+                            break;
                     }
                 }
             } //opcion CONSULTAR
